7d12cff35c4c2174acf0a818fb53bb633a613edd
Cromuel Barut <cromuelbarut@gmail.com>
2020-06-03 00:00:43 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+   // get year for the copyright year
+   $('#year').text(new Date().getFullYear());
+
+   // configure superslides
+   $('#slides').superslides({
+      play: 10000,
+      animation: 'fade',
+      pagination: false
+   });
+   $('#about-slides').superslides({
+      play: 5000,
+      animation: 'slide',
+      pagination: true
+   });
+
+   // CHANGES NAVBAR WHEN SCROLL
+   window.addEventListener('scroll', function () {
+      if (this.scrollY < 100) {
+         document.querySelector('#main-nav').classList.remove('bg-dark');
+         document.querySelector('#main-nav').classList.remove('navbar-shadow');
+         document.querySelector('#main-nav').classList.remove('navbar-border');
+      } else if (this.scrollY >= 100 && this.scrollY < window.innerHeight - 62) {
+         document.querySelector('#main-nav').classList.remove('bg-dark');
+         document.querySelector('#main-nav').classList.add('navbar-shadow');
+         document.querySelector('#main-nav').classList.remove('navbar-border');
+      } else if (this.scrollY >= window.innerHeight - 62) {
+         document.querySelector('#main-nav').classList.add('bg-dark');
+         document.querySelector('#main-nav').classList.remove('navbar-shadow');
+         document.querySelector('#main-nav').classList.add('navbar-border');
+      }
+   });
+
+   /* SMOOTH SCROLLING */
+   $('#main-nav a, .btn').on('click', function (event) {
+      if (this.hash !== '') {
+         event.preventDefault();
+
+         const hash = this.hash;
+
+         $('html, body').animate(
+            {
+               scrollTop: $(hash).offset().top - 100
+            },
+            800
+         );
+      }
+   });
+});
