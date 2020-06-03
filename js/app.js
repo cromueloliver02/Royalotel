615eed58fc0f223fc401af9343f42cd56501b4b7
Cromuel Barut <cromuelbarut@gmail.com>
2020-06-03 21:17:20 +0800

    added designs

--- js/app.js
+++ js/app.js
@@ -48,7 +48,7 @@ $(document).ready(function () {
    });
 
    new Glider(document.querySelector('.glider'), {
-      slidesToShow: 3,
+      slidesToShow: 3.4,
       slidesToScroll: 1,
       arrows: {
          prev: '.glider-prev',
@@ -58,4 +58,24 @@ $(document).ready(function () {
       draggable: true
 
    })
+
+   // lightbox
+   $(document).on('click', '[data-toggle="lightbox"]', function (event) {
+      event.preventDefault();
+      $(this).ekkoLightbox();
+   });
+
+   // Video Play
+   $(function () {
+      // Auto play modal video
+      $(".video").click(function () {
+         var theModal = $(this).data("target"),
+            videoSRC = $(this).attr("data-video"),
+            videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+         $(theModal + ' iframe').attr('src', videoSRCauto);
+         $(theModal + ' button.close').click(function () {
+            $(theModal + ' iframe').attr('src', videoSRC);
+         });
+      });
+   });
 });
